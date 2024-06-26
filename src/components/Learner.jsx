58476f31c54048e1c4d7ea6477e6b037dc82a8be
Said Masih <saidmsajady@gmail.com>
2024-06-26 11:45:34 -0700

    Finished Lab

---
+++ src/components/Learner.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Learner({ name, bio, scores }) {
+  return (
+    <div>
+
+        <h2>{name}</h2>
+        <p>{bio}</p>
+
+        <ul>
+        {scores.map((score, index) => (
+            <li key={index}>{score.date}: {score.score}</li>
+        ))}
+        </ul>
+
+    </div>
+  )
+}
